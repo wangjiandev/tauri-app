4736f7546723f1fdaa9c210d12508f5c6558ac49
Wang Jian <wangjian0504@gmail.com>
2026-04-14 10:34:42 +0800

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 function App() {
   return (
-    <main className="container">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
+    <div className="flex min-h-svh flex-col items-center justify-center p-2">
+      <Button>Click me</Button>
+      <Field>
+        <FieldLabel htmlFor="input-demo-api-key">API Key</FieldLabel>
+        <Input id="input-demo-api-key" type="password" placeholder="sk-..." />
+        <FieldDescription>
+          Your API key is encrypted and stored securely.
+        </FieldDescription>
+      </Field>
+    </div>
   );
 }
 
